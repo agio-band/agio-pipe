381727afcbd13cd833ba43105ecb65545bc4c589
paulwinex <paulwinex@gmail.com>
2025-08-11 12:44:19 +0300

    update settings

--- agio_pipe/package_settings/local_settings.py
+++ agio_pipe/package_settings/local_settings.py
@@ -1,7 +1,14 @@
 from typing import Optional
+from pydantic import BaseModel
 
 from agio.core.settings.package_settings import APackageSettings
 
 
+class LocalRootsSettings(BaseModel):
+    name: str
+    path: str
+
+
 class PipeLocalSettings(APackageSettings):
     publish_temp_dir: Optional[str] = None
+    local_roots: list[LocalRootsSettings]
