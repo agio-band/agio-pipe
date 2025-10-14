ed8aa3e17c90f253feacb1ed725a98888707dbc1
paulwinex <paulwinex@gmail.com>
2025-10-14 11:27:36 +0300

    refactor callbacks

--- agio_pipe/plugins/callbacks.py
+++ agio_pipe/plugins/callbacks.py
@@ -1,14 +1,14 @@
 import logging
-from agio.core.events import callback
+from agio.core.events import callback, AEvent
 from agio.core.pkg import APackageManager
 import importlib
 
 logger = logging.getLogger(__name__)
 
 @callback('core.app.all_packages_loaded')
-def package_loaded(event, package_hub):
+def package_loaded(event: AEvent):
     logger.debug("Load entity classes")
-    for pkg in package_hub.iter_packages():
+    for pkg in event.payload['package_hub'].iter_packages():
         pkg: APackageManager
         entities_path = pkg.get_meta_data_field('project_entities_path', 'entities/*.py')
         files = list(pkg.root.glob(entities_path))
