91d5dc037905e81f70bae24bde89ee5091ca1088
paulwinex <paulwinex@gmail.com>
2026-02-14 16:06:32 +0300

    refactor settings

--- agio_pipe/package_settings/workspace_settings.py
+++ agio_pipe/package_settings/workspace_settings.py
@@ -1,15 +1,22 @@
 from typing import Optional
 
-from agio.core.settings import APackageSettings, JSONField, PluginSelectField
+from agio.core.settings import APackageSettings, JSONField, PluginSelectField, ListField
 from pydantic import BaseModel, Field
 
 from agio.core.settings.fields.special_fields import ChipSelectField
 
+TemplateType = dict[str, str]
+
 
 class PublishTemplate(BaseModel):
     name: str
-    # root_name: str = None
-    pattern: str = Field(..., widget='PathTemplate')
+    patterns: TemplateType = Field(..., json_schema_extra=dict(widget='PathTemplate'))
+    variables: dict[str, str] = Field(default_factory=dict)
+
+
+class ReviewTemplates(BaseModel):
+    name: str
+    template: dict
 
 
 class PipeWorkspaceSettings(APackageSettings):
@@ -17,9 +24,9 @@ class PipeWorkspaceSettings(APackageSettings):
     publish_plugin: str = PluginSelectField('publish_engine')
     publish_scene_chip: str = ChipSelectField('publish_scene', default='default')
     # templates
-    publish_templates: Optional[list[PublishTemplate]] = []
     apply_burn_in: bool = True
-    review_template: str = JSONField(...)
+    review_templates: Optional[list[ReviewTemplates]] = ListField(default=list)
     # publish processing
+    publish_templates: Optional[list[PublishTemplate]] = ListField(default=list)
     align_product_versions: bool = False
 
