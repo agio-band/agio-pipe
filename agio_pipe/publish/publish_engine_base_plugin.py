c07a8ca0804124427693ee1089e2a383a7eaf02e
paulwinex <paulwinex@gmail.com>
2026-03-15 20:15:06 +0300

    add support of custom ui for publish engine

--- agio_pipe/publish/publish_engine_base_plugin.py
+++ agio_pipe/publish/publish_engine_base_plugin.py
@@ -4,6 +4,7 @@ from agio_pipe.publish.publish_session import PublishSession
 
 class PublishEngineBasePlugin(APlugin):
     plugin_type = 'publish_engine'
+    open_ui_function = None
     parameters = None
 
     def execute(self, session: PublishSession, **options) -> None:
